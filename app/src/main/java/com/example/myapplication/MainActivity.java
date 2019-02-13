414e78bb67372d76ab43f3c3855065a73bf804b5
e1211303 <e1211303@niihama.kosen-ac.jp>
2019-02-13 15:43:27 +0900

    Buttonのリスナ設定

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity
@@ -12,10 +13,25 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ボタンにリスナを設定する処理
+        setOnClickListeners();
+    }
+
+    //配置済みのボタンにリスナを設定する
+    private void setOnClickListeners(){
+
+        //配置されたButtonを取得
+        Button count_button = findViewById(R.id.button_count_up);
+        Button reset_button = findViewById(R.id.button_reset);
+
+        //MainActivity自身をリスナとして設定する
+        count_button.setOnClickListener(this);
+        reset_button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        //ここに実際の処理を記述する
+
     }
 }
